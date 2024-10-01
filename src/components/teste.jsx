82adf2c8ac392bb4b76eee80badb8e928e6307a8
Christopher Willians <163358050+christopherfifo@users.noreply.github.com>
2024-10-01 03:20:49 -0300

    first

---
+++ src/components/teste.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Teste() {
+    let a=1;
+    let b=2;
+    let c=a+b;
+    return (
+      <h1 className="text-3xl font-bold underline">
+        opaaaa!{c}
+      </h1>
+    )
+  }
