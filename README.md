980f8dd50e32ac5cf25cf6ff399947c61dffa647
Christopher Willians <163358050+christopherfifo@users.noreply.github.com>
2024-10-03 20:39:34 -0300

    react icons

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# react
+# libs
+react icons
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//! importação das pages
 import './App.css'
 import Teste from './components/teste.jsx'
+//& importação de icones
+import {FaHeart} from 'react-icons/fa'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,21 +11,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR <FaHeart/>
         </p>
       </div>
       <p className="read-the-docs">
